ac729f772a019c2be31abccca3bb95d292e5f3b1
Jayesh <jayesh.jspatil@gmail.com>
2023-02-02 09:23:01 +0530

    adding move negative aside code

---
+++ src/com/level1/MoveNegative.java
@@ -0,0 +1,15 @@
+package com.level1;
+
+import java.util.stream.IntStream;
+
+public class MoveNegative {
+
+	public static void main(String[] args) {
+		int[] arr= {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+		
+		IntStream.of(arr).sorted().forEach(s->System.out.print(s+" "));
+		
+
+	}
+
+}
