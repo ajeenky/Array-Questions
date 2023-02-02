606bb73289234f3d76caf6d313dc6fe22d5dc2fe
sudip <manjarekarsudip@gmail.com>
2023-02-02 18:50:08 +0530

    Maximum & Minimum Element of Array

--- src/com/velocity/MaxAndMinNumInArray.java
+++ src/com/velocity/MaxAndMinNumInArray.java
@@ -3,8 +3,24 @@ package com.velocity;
 public class MaxAndMinNumInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a[] = {2,6,4,1,7,9,3};
+		int max = a[0];
+		int min = a[0];
+		
+		for(int i = 0; i<a.length ; i++) {
+			if(a[i]>max) {
+				max = a[i];
+			}
+		}
+		System.out.println("Maximum number is : " + max);
+		
+		for(int i = 0; i<a.length ;i++) {
+			if(a[i]<min) {
+				min = a[i];
+			}
+		}
+		System.out.println("Minimum number is : "+ min);
 	}
 
 }
