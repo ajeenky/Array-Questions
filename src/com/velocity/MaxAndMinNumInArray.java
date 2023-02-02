ef48b3ae7bc2c11ab2065f5b72b8efe0d022e9ff
chinmay <chinmay29.shende@gmail.com>
2023-02-02 21:38:33 +0530

    Done

--- src/com/velocity/MaxAndMinNumInArray.java
+++ src/com/velocity/MaxAndMinNumInArray.java
@@ -3,7 +3,18 @@ package com.velocity;
 public class MaxAndMinNumInArray {
 
 	public static void main(String[] args) {
-		
+		int[] arr = {65,53,75,68};
+		int max = arr[0];
+		int min = arr[0];
+		for(int i =0;i<arr.length;i++) {	
+			if(arr[i]>max) {
+				max = arr[i];
+			}
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+		}System.out.println(max);
+		 System.out.println(min);
 
 	}
 
