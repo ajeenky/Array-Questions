c0df4403c44da6acdc62a3c0d181d5919f085d8e
AjaySwami <akjangam007@gmail.com>
2023-02-02 23:03:02 +0530

    trial

--- src/com/velocity/MaxAndMinNumInArray.java
+++ src/com/velocity/MaxAndMinNumInArray.java
@@ -3,7 +3,10 @@ package com.velocity;
 public class MaxAndMinNumInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int []a= {1,2,3,4,5,6};
+		int i;
+		
+		
 
 	}
 
