c0537aac2e5c2f49d53622651fab318f6d8bf283
priyanka <priyankagulbhile@gmail.com>
2023-02-03 14:46:10 +0530

    min and max printed successfully

--- src/com/velocity/MaxAndMinNumInArray.java
+++ src/com/velocity/MaxAndMinNumInArray.java
@@ -2,9 +2,24 @@ package com.velocity;
 
 public class MaxAndMinNumInArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) 
+	{
+		int array[] = {2,1,4,3,5,6};
+		int max = array[0];
+		int min = array[0];
+		for(int i=1;i<array.length;i++) 
+		{
+			if(array[i]>max) 
+			{
+			max = array[i];
+		    }
+			if(array[i]<min)
+			{
+				min = array[i];
+			}
+			System.out.println("max number is::"+max);
+			System.out.println("min number is::"+min);
+		}
+	
+	}		
 }
