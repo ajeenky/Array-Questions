66c9fc10b4a08c7533954190ce369657e947858d
Shinde1001 <pradipshinde.v@gmail.com>
2023-02-04 23:29:44 +0530

    Reverse Array Class

---
+++ src/com/velocity/ReverseOfArray.java
@@ -0,0 +1,31 @@
+package com.velocity;
+
+public class ReverseOfArray {
+
+		public int [] getReverseArray(int[]arr) {
+			int l = arr.length;
+			int j =0;
+			int [] result=new int [l];
+			for(int i=l-1;i>=0;i--)//8-->
+			{
+					result[j]=arr[i];
+					j++;
+			}
+			
+			return result;
+		}
+		
+		public static void main(String[] args) {
+			 ReverseOfArray reArray = new ReverseOfArray();
+			 int [] array = {1,2,3,4,5,6,9,8,7};
+			 int[] array2 = reArray.getReverseArray(array);
+			  
+			 System.out.print("[ "); 
+			 for(Integer x:array2) {
+				  System.out.print(x+" ");
+			  }
+			 System.out.print("]");
+		}
+
+	}
+
