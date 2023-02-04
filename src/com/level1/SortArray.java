7115eb16be55b810cabacddd0f2cc51153d7b250
Shinde1001 <pradipshinde.v@gmail.com>
2023-02-05 01:46:11 +0530

    Sorted array modified

--- src/com/level1/SortArray.java
+++ src/com/level1/SortArray.java
@@ -1,22 +1,25 @@
 package com.level1;
 
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
+ 
+ 
 
 public class SortArray {
 	
-	public TreeSet<Integer> getSortedArray(int [] arr){
-		TreeSet<Integer> result = new TreeSet<Integer>();
+	public ArrayList<Integer> getSortedArray(int [] arr){
+		ArrayList<Integer> result = new ArrayList<Integer>();
 		int l = arr.length;
-		for(int i=0;i<l-1;i++) {
+		for(int i=0;i<l;i++) {
 			 result.add(arr[i]);
 		}
-		
+		Collections.sort(result);
 		return result;
 	}
 	public static void main(String[] args) {
-		 int[] array = {4,5,6,8,3,6,9,1,2,7};
+		 int[] array = {4,5,6,8,6,3,6,9,1,2,7};
 		 SortArray sortArray = new SortArray();
-		 TreeSet<Integer> set = sortArray.getSortedArray(array);
+		 ArrayList<Integer> set = sortArray.getSortedArray(array);
 		 System.out.println(set);
 	}
 
