7923fc14d10e2339609b9f2ba850cb8103e736d3
akshata <sagvekar18akshata@gmail.com>
2023-02-05 12:29:42 +0530

    MinMaxArray class added

--- src/com/velocity/MinMaxArray.java
+++ src/com/velocity/MinMaxArray.java
@@ -4,7 +4,23 @@ public class MinMaxArray {
 
 	public static void main(String[] args) {
 		
-
+		int arr[] = {5,8,6,2,3,9,10,15};
+		
+		int max = arr[0];
+		int min = arr[0];
+		
+		for(int i = 0; i<arr.length; i++)
+		{
+			if(arr[i]>max)
+			{
+				max = arr[i];
+			}
+			if(arr[i]<min)
+			{
+				min = arr[i];
+			}
+		}
+		System.out.println("maximum number is : " +max);
+		System.out.println("minimum number is : " +min);
 	}
-
 }
