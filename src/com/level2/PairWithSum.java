93eb256e4f7463cbe8590b3acc56b662abc8530a
Jayesh <jayesh.jspatil@gmail.com>
2023-02-05 10:48:26 +0530

    add count pair with sum code

---
+++ src/com/level2/PairWithSum.java
@@ -0,0 +1,19 @@
+package com.level2;
+
+public class PairWithSum {
+	//Count Pairs with given sum
+	// arr[] = {1, 5, 7, -1}, sum = 6
+	public static void main(String[] args) {
+	int  arr[] = {1, 5, 7, -1,1,5,4,2};
+	int sum=6;
+	int count=0;
+	for(int i=0;i<arr.length;i++) {
+		for(int j=i;j<arr.length;j++) {
+			if(arr[i]+arr[j]==sum) {
+				count++;
+			}
+		}		
+	}
+	System.out.println(count);	
+	}
+}
