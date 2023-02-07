5219add6582afc350fa27fcaaa21ce1f94735e24
shweta <shwetashinde875@gmail.com>
2023-02-07 18:22:05 +0530

    update

---
+++ src/com/velocity/Sort1s2s.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.Arrays;
+
+public class Sort1s2s {
+
+	public static void main(String[] args) {
+		int[] arr= {1,1,0,2,0,2,1,0,2,1,0,1,2,0,1,2};
+		Arrays.sort(arr);
+		for(int x:arr) {
+			System.out.print(x);
+		}
+		
+
+	}
+
+}
---
+++ src/com/velocity/UnionIntersection.java
@@ -0,0 +1,43 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+// find union and intersection of array
+public class UnionIntersection {
+
+	public static void union(int[] arr1, int[] arr2) {
+		Set<Integer> union = new HashSet<Integer>();
+		for (int i : arr1) {
+			union.add(i);
+		}
+		for (int i : arr2) {
+			union.add(i);
+		}
+		System.out.println("union of array");
+		for (int i : union) {
+			System.out.print(i + "  ");
+		}
+	}
+
+	public static void intersection(int[] arr1, int[] arr2) {
+		Set<Integer> intersection = new HashSet<Integer>();
+		for (int i : arr1) {
+			for (int j : arr2) {
+				intersection.add(i);
+
+			}
+		}
+		System.out.println("intersection of array");
+		for (int x : intersection) {
+			System.out.print(x + "  ");
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr1[] = { 1, 2, 3, 4, 5, 7, 8, 9 };
+		int arr2[] = { 4, 5, 6, 7, 10, 12, 11 };
+		union(arr1, arr2);
+		intersection(arr1, arr2);
+	}
+
+}
