8908a1845856f65c5fac1bac54efda348e530b39
saurabh <saurabh16.shende@gmail.com>
2023-02-14 17:00:39 +0530

    updated

--- src/com/array/saurabh/MaxMin.java
+++ src/com/array/saurabh/MaxMin.java
@@ -3,8 +3,21 @@ package com.array.saurabh;
 public class MaxMin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a[] = {12, 16, 8, 4, 40, 26, 5};
+			int max = a[0]; 
+			int min = a[0];
+			
+			for(int i = 0; i < a.length; i++) {
+				if(a[i] > max) {
+					max = a[i];
+				}
+				if(a[i] < min) {
+					min = a[i];
+				}
+			}
+			System.out.println("Largest element of array is : " + max);
+			System.out.println("Smallest element of array is : "+ min);
 	}
 
 }
