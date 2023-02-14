2d513c4912929d3a7a3e32975e477394f8a15d2f
saurabh <saurabh16.shende@gmail.com>
2023-02-14 17:14:02 +0530

    reverse  done

--- src/com/array/saurabh/MaxMin.java
+++ src/com/array/saurabh/MaxMin.java
@@ -1,23 +1,27 @@
 package com.array.saurabh;
+//To print the max & min element of an array:
 
 public class MaxMin {
 
 	public static void main(String[] args) {
-		
-		int a[] = {12, 16, 8, 4, 40, 26, 5};
-			int max = a[0]; 
-			int min = a[0];
-			
-			for(int i = 0; i < a.length; i++) {
-				if(a[i] > max) {
-					max = a[i];
-				}
-				if(a[i] < min) {
-					min = a[i];
-				}
-			}
-			System.out.println("Largest element of array is : " + max);
-			System.out.println("Smallest element of array is : "+ min);
-	}
 
+		int a[] = { 12, 16, 8, 4, 40, 26, 5 };
+		
+		int max = a[0];										//Assumption
+		int min = a[0];										//Assumption
+
+		for (int i = 0; i < a.length; i++) {
+			
+			if (a[i] > max) {									//Check for Maximum
+				max = a[i];
+			}
+			
+			if (a[i] < min) {									//Check for Minimum
+				min = a[i];
+			}
+		}
+		
+		System.out.println("Largest element of array is : " + max);
+		System.out.println("Smallest element of array is : " + min);
+	}
 }
--- src/com/array/saurabh/PeakElement.java
+++ src/com/array/saurabh/PeakElement.java
@@ -1,16 +1,16 @@
 package com.array.saurabh;
-
 //Find Peak Element in Array
-public class PeakElement {
+//An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
 
+public class PeakElement {
 	public static void main(String[] args) {
 		int[] a = { 5, 4, 13, 9, 20, 16, 8, 11, 15, 21 };
 		for (int i = 0; i < a.length; i++) {
-			if (i == 0 && a[i] > a[i + 1]) {
+			if (i == 0 && a[i] > a[i + 1]) {															//To check first element
 				System.out.println(a[i]);
-			} else if (i == a.length - 1 && a[a.length - 1] > a[a.length - 2]) {
+			} else if (i == a.length - 1 && a[a.length - 1] > a[a.length - 2]) {		//To check last element
 				System.out.println(a[a.length - 1]);
-			} else if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+			} else if (a[i] > a[i - 1] && a[i] > a[i + 1]) {										//To check remaining elements
 				System.out.println(a[i]);
 			}
 		}
